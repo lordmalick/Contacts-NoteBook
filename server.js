72536276be5e469a7a62555702d3bd0ef3d3cb92
Malick <fallmalick99@gmail.com>
2022-10-03 16:51:03 +0000

    changing server port number

--- server.js
+++ server.js
@@ -9,7 +9,6 @@ const connectDB = require('./server/database/connection');
 const app = express();
 
 dotenv.config( { path : 'config.env'} )
-const PORT = process.env.PORT || 8080
 
 // log requests
 app.use(morgan('tiny'));
@@ -32,4 +31,5 @@ app.use('/js', express.static(path.resolve(__dirname, "assets/js")))
 // load routers
 app.use('/', require('./server/routes/router'))
 
-app.listen(PORT, ()=> { console.log(`Server is running on http://localhost:${PORT}`)});
+app.listen(process.env.PORT || 3000, 
+	() => console.log("Server is running..."));
